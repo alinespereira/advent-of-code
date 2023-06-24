978dec6443607626b37d6ab5f3ca27df0b936b79
Aline <line.asramos@gmail.com>
2023-06-24 20:28:34 -0300

    All days' structure

---
+++ src/bin/runner.rs
@@ -0,0 +1,13 @@
+use advent_of_code::Day;
+
+const AVAILABLE_YEARS: &[usize] = &[2022];
+
+macro_rules! make_aoc_day {
+    ($year:expr, $day:expr) => {};
+}
+
+fn main() {
+    for year in AVAILABLE_YEARS {
+        for day in 1..=25 {}
+    }
+}
---
+++ src/year_2022/day_08/mod.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+use crate::{AdventOfCodeSolver, Day};
+
+impl AdventOfCodeSolver for Day<2022, 8> {
+    fn solve_part_one(&mut self, input: String) {
+        todo!();
+    }
+
+    fn solve_part_two(&mut self, input: String) {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(unused)]
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn it_solves_part_one() {
+        let mut day: Day<2022, 8> = Day::new();
+        todo!();
+    }
+
+    #[test]
+    fn it_solves_part_two() {
+        let mut day: Day<2022, 8> = Day::new();
+        todo!();
+    }
+}
