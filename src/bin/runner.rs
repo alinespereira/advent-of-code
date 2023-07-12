720ce4129519da1c9543ab395649acd5ebd82ee3
Aline <line.asramos@gmail.com>
2023-07-12 11:59:42 -0300

    Refactor runner and set as default bin

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -1,32 +1,41 @@
 use advent_of_code::{AdventOfCodeSolver, Day};
-use std::fs;
+
+#[macro_export]
+macro_rules! run_aoc {
+    ($year:literal/$day:literal) => {
+        let mut aoc = Day::<$year, $day>::new();
+        let path = aoc.get_input_path();
+        let input: String = std::fs::read_to_string(path).unwrap();
+        aoc.solve_part_one(input.clone());
+        aoc.solve_part_two(input.clone());
+        println!("{}", aoc);
+    };
+}
 
 fn main() {
-    let mut aoc = Day::<2022, 1>::new();
-    let path = aoc.get_input_path();
-    let input: String = fs::read_to_string(path).unwrap();
-    aoc.solve_part_one(input.clone());
-    aoc.solve_part_two(input.clone());
-    println!("{}", aoc);
-
-    let mut aoc = Day::<2022, 2>::new();
-    let path = aoc.get_input_path();
-    let input: String = fs::read_to_string(path).unwrap();
-    aoc.solve_part_one(input.clone());
-    aoc.solve_part_two(input.clone());
-    println!("{}", aoc);
-
-    // let mut aoc = Day::<2022, 3>::new();
-    // let path = aoc.get_input_path();
-    // let input: String = fs::read_to_string(path).unwrap();
-    // aoc.solve_part_one(input.clone());
-    // aoc.solve_part_two(input.clone());
-    // println!("{}", aoc);
-
-    // let mut aoc = Day::<2022, 4>::new();
-    // let path = aoc.get_input_path();
-    // let input: String = fs::read_to_string(path).unwrap();
-    // aoc.solve_part_one(input.clone());
-    // aoc.solve_part_two(input.clone());
-    // println!("{}", aoc);
+    run_aoc!(2022 / 1);
+    run_aoc!(2022 / 2);
+    run_aoc!(2022 / 3);
+    run_aoc!(2022 / 4);
+    // run_aoc!(2022 / 5);
+    // run_aoc!(2022 / 6);
+    // run_aoc!(2022 / 7);
+    // run_aoc!(2022 / 8);
+    // run_aoc!(2022 / 9);
+    // run_aoc!(2022 / 10);
+    // run_aoc!(2022 / 11);
+    // run_aoc!(2022 / 12);
+    // run_aoc!(2022 / 13);
+    // run_aoc!(2022 / 14);
+    // run_aoc!(2022 / 15);
+    // run_aoc!(2022 / 16);
+    // run_aoc!(2022 / 17);
+    // run_aoc!(2022 / 18);
+    // run_aoc!(2022 / 19);
+    // run_aoc!(2022 / 20);
+    // run_aoc!(2022 / 21);
+    // run_aoc!(2022 / 22);
+    // run_aoc!(2022 / 23);
+    // run_aoc!(2022 / 24);
+    // run_aoc!(2022 / 25);
 }
